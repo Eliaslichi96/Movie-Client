190587081767e509c16ddde0880d5760be4067f4
Eliaslichi96 <146300075+Eliaslichi96@users.noreply.github.com>
2024-02-28 21:14:18 -0500

    3.4

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,16 +7,61 @@ export const MainView = () => {
 
     const [selectedMovie, setSelectedMovie] = useState(null);
 
+    useEffect(() => {
+        fetch("https://careerfoundry-movieflix-59ee318aca62.herokuapp.com/movieshttps://movie-api-project14-def5aeaaa6a3.herokuapp.com/movies")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("movies from api:", data);
+                const moviesFromApi = data.map((movie) => {
+                    return {
+                        _id: movie._id,
+                        Title: movie.Title,
+                        ImagePath: movie.ImagePath,
+                        Description: movie.Description,
+                        Genre: {
+                            Name: movie.Genre.Name,
+                        },
+                        Director: {
+                            Name: movie.Director.Name,
+                        },
+                    };
+                });
+                setMovies(moviesFromApi);
+            });
+    }, []);
+
     if (selectedMovie) {
-        return (
-            <MovieView
-                movie={selectedMovie}
-                onBackClick={() => {
-                    setSelectedMovie(null);
-                }}
-            />
-        );
+        let similarMovies = movies.filter((movie) => {
+            return movie._id !== selectedMovie._id && movie.Genre.Name === selectedMovie.Genre.Name;
+        });
+        if (similarMovies.length === 0) {
+            return (
+                <>
+                    <MovieView movie={selectedMovie} onBackClick={() => setselectedMovie(null)} /><br />
+                    <h2>Similar Movies</h2>
+                    <p>There are no similar movies.</p>
+                </>
+            );
+        } else {
+            return (
+                <>
+                    <MovieView movie={selectedMovie} onBackClick={() => setselectedMovie(null)} /><br />
+                    <h2>Similar Movies</h2>
+                    {similarMovies.map((movie) => (
+                        <MovieCard
+                            key={movie._id}
+                            movie={movie}
+                            onMovieClick={(newSelectedMovie) => {
+                                setselectedMovie(newSelectedMovie);
+                            }}
+                        />
+                    ))}
+                </>
+            );
+        }
     }
+
+
 
     if (movies.length === 0) {
         return <div>The list is empty!</div>;
@@ -25,7 +70,7 @@ export const MainView = () => {
         <div>
             {movies.map((movie) => (
                 <MovieCard
-                    key={movie.id}
+                    key={movie._id}
                     movie={movie}
                     onMovieClick={(newSelectedMovie) => {
                         setSelectedMovie(newSelectedMovie);
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,7 @@
+// Here you import the PropTypes library
 import PropTypes from "prop-types";
 
+// The MovieCard function component
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div
@@ -11,9 +13,11 @@ export const MovieCard = ({ movie, onMovieClick }) => {
         </div>
     );
 };
+
+// Here is where we define all the props constraints for the MovieCard
 MovieCard.propTypes = {
     movie: PropTypes.shape({
-        title: PropTypes.string.isRequired,
+        Title: PropTypes.string,
     }).isRequired,
     onMovieClick: PropTypes.func.isRequired,
 };
