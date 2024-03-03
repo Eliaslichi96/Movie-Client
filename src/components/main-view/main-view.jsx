8483ea3748b209590191c3ea8d1c024fb7ac0bc5
Eliaslichi96 <146300075+Eliaslichi96@users.noreply.github.com>
2024-03-03 15:07:49 -0500

    3.4 start

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SingupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([
