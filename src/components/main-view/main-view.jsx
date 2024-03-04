5da5ccbb071deae9d1a9db561332d708b19b0995
Eliaslichi96 <146300075+Eliaslichi96@users.noreply.github.com>
2024-03-03 21:28:46 -0500

    3.5 login/signup

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,20 +1,31 @@
 import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SingupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+    const storedUser = localStorage.getItem("user");
+    const storedToken = localStorage.getItem("token");
+
+    const [user, setUser] = useState(storedUser ? storedUser : null);
+    const [token, setToken] = us
     const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
 
     useEffect(() => {
-        fetch("https://movie-api-project14-def5aeaaa6a3.herokuapp.com/movies")
+        if (!token) {
+            return;
+        }
+        fetch("https://movie-api-project14-def5aeaaa6a3.herokuapp.com/movies", {
+            headers: { Authorization: `Bearer ${token}` },
+        })
             .then((response) => response.json())
             .then((data) => {
-                console.log("movies from api:", data);
                 const moviesFromApi = data.map((movie) => {
                     return {
-                        _id: movie._id,
+                        id: movie._id,
                         Title: movie.title,
                         ImagePath: movie.ImagePath,
                         Description: movie.Description,
@@ -27,8 +38,25 @@ export const MainView = () => {
                     };
                 });
                 setMovies(moviesFromApi);
+
             });
-    }, []);
+    },
+        [token]);
+
+    if (!user) {
+        return (
+            <>
+                <LoginView
+                    onLoggedIn={(user, token) => {
+                        setUser(user);
+                        setToken(token);
+                    }}
+                />
+                or
+                <SingupView />
+            </>
+        );
+    }
 
     if (selectedMovie) {
         let similarMovies = movies.filter((movie) => {
@@ -77,6 +105,15 @@ export const MainView = () => {
                     }}
                 />
             ))}
+            <button
+                onClick={() => {
+                    setUser(null);
+                    setToken(null);
+                    localStorage.clear();
+                }}
+            >
+                Logout
+            </button>
         </div>
     );
 };
