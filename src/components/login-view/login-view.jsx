2d5e55ff529633e45b9f2b4c304f5913069d9c21
Eliaslichi96 <146300075+Eliaslichi96@users.noreply.github.com>
2024-03-17 14:05:05 -0400

    3.7

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -39,7 +39,7 @@ export const LoginView = ({ onLoggedIn }) => {
     };
 
     return (
-        <Form onSubmit={handleSubmit} className="mt-5>
+        <Form onSubmit={handleSubmit} className="mt-5">
             <h1> Login to your account </h1>
             <Form.Group controlId="formUsername">
         < Form.Label > Username:</Form.Label >
